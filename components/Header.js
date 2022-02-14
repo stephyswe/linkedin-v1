927f261d7cea94290164228c8ac505496732f3fa
Stephanie <stephaniiehallberg@gmail.com>
2022-02-14 02:21:51 +0100

    dark mode toggle

--- components/Header.js
+++ components/Header.js
@@ -1,6 +1,8 @@
+import { useEffect, useState } from 'react'
 import Image from 'next/image'
+import { useTheme } from 'next-themes'
+import { motion } from 'framer-motion'
 import SearchRoundedIcon from '@mui/icons-material/SearchRounded'
-import HeaderLink from './HeaderLink'
 import GroupIcon from '@mui/icons-material/Group'
 import BusinessCenterIcon from '@mui/icons-material/BusinessCenter'
 import ChatIcon from '@mui/icons-material/Chat'
@@ -8,30 +10,48 @@ import NotificationsIcon from '@mui/icons-material/Notifications'
 import HomeRoundedIcon from '@mui/icons-material/HomeRounded'
 import AppsOutlinedIcon from '@mui/icons-material/AppsOutlined'
 import { Avatar } from '@mui/material'
-import { useEffect, useState } from 'react'
-// import { useTheme } from "next-themes";
-// import { motion } from "framer-motion";
+import HeaderLink from './HeaderLink'
+
+const spring = {
+  type: 'spring',
+  stiffness: 700,
+  damping: 30
+}
 
 function Header() {
+  const [mounted, setMounted] = useState(false)
+  const { setTheme, resolvedTheme, theme } = useTheme()
+
+  // After mounting, we have access to the theme
+  useEffect(() => setMounted(true), [])
+
+  console.log('Current theme is', theme)
   return (
-    <header>
+    <header
+      className="sticky top-0 z-40 bg-white dark:bg-[#1D2226]
+     flex items-center justify-around py-1.5 px-3 focus-within:shadow-lg"
+    >
       {/*left */}
       <div className="flex items-center space-x-2 w-full max-w-xs">
-        <Image src="https://rb.gy/bizvqj" width={45} height={45} />
+        {mounted && (
+          <>
+            {resolvedTheme === 'dark' ? (
+              <Image src="https://rb.gy/bizvqj" width={45} height={45} />
+            ) : (
+              <Image src="https://rb.gy/dpmd9s" width={55} height={55} />
+            )}
+          </>
+        )}
 
-        <div
-          className="flex items-center space-x-1 
-        dark:md:bg-gray-700 py-2.5 px-4 rounded w-full"
-        >
+        <div className="flex items-center space-x-1 dark:md:bg-gray-700 py-2.5 px-4 rounded w-full dark:pl-6">
           <SearchRoundedIcon />
           <input
             type="text"
             placeholder="Search"
-            className="hidden md:inline-flex bg-transparent text-sm
-             focus:outline-none placeholder-black/70 dark:placeholder-white/75 flex-grow"
+            className="hidden md:inline-flex bg-transparent text-sm focus:outline-none 
+            placeholder-black/70 dark:placeholder-white/75 flex-grow"
           />
         </div>
-        <Image src="https://rb.gy/dpmd9s" width={55} height={55} />
       </div>
 
       {/*right */}
@@ -43,6 +63,27 @@ function Header() {
         <HeaderLink Icon={NotificationsIcon} text="Notifications" feed />
         <HeaderLink Icon={Avatar} text="Me" feed avatar hidden />
         <HeaderLink Icon={AppsOutlinedIcon} text="Work" feed hidden />
+
+        {/* Dark mode toggle */}
+        {mounted && (
+          <div
+            className={`bg-gray-600 flex items-center px-0.5 rounded-full h-6 w-12 cursor-pointer 
+            flex-shrink-0 relative ${
+              resolvedTheme === 'dark' ? 'justify-end' : 'justify-start'
+            }`}
+            onClick={() =>
+              setTheme(resolvedTheme === 'dark' ? 'light' : 'dark')
+            }
+          >
+            <span className="absolute left-0">🌜</span>
+            <motion.div
+              className="w-5 h-5 bg-white rounded-full z-40"
+              layout
+              transition={spring}
+            />
+            <span className="absolute right-0.5">🌞</span>
+          </div>
+        )}
       </div>
     </header>
   )
