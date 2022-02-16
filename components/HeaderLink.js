b18dcb60bae11a1b2f2ce119ed4a480e72dc2651
Stephanie <stephaniiehallberg@gmail.com>
2022-02-16 02:36:38 +0100

    fix - signout in header

--- components/HeaderLink.js
+++ components/HeaderLink.js
@@ -1,4 +1,8 @@
-function HeaderLink({ Icon, text, avatar, feed, active, hidden }) {
+import { useSession, signOut } from 'next-auth/react'
+
+function HeaderLink({ Icon, text, feed, active, avatar, hidden }) {
+  const { data: session } = useSession()
+
   return (
     <div
       className={`${
@@ -8,8 +12,13 @@ function HeaderLink({ Icon, text, avatar, feed, active, hidden }) {
           ? 'text-black/60 hover:text-black dark:text-white/75 dark:hover:text-white lg:-mb-1.5 space-y-1'
           : 'text-gray-500 hover:text-gray-700'
       } ${active && '!text-black dark:!text-white'}`}
+      onClick={() => avatar && signOut()}
     >
-      {avatar ? <Icon className="!h-7 !w-7 lg:!-mb-1" /> : <Icon />}
+      {avatar ? (
+        <Icon className="!h-7 !w-7 lg:!-mb-1" src={session?.user?.image} />
+      ) : (
+        <Icon />
+      )}
 
       <h4
         className={`text-sm ${
